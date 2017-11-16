ebc48638ef4944808cf576fa81cf7e542e976f56
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-16 17:33:30 +0900

    Update redirect.php

--- www/redirect.php
+++ www/redirect.php
@@ -139,7 +139,7 @@ foreach ($pdo->query($sql) as $row)
       'header' => array(
         'Content-Type: text/plain'
         ),
-      'content' => "${row['fqdn']} ${row['update_time']} ${row['dyno_used']} ${row['dhm']}${row['note']}${row['state']}"
+      'content' => "${row['dhm']} ${row['fqdn']} ${row['update_time']} ${row['dyno_used']}${row['note']}${row['state']}"
       ));
   $res = file_get_contents($url, false, stream_context_create($context));
 }
