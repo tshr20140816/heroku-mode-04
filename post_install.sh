0ee0baaa0ce7c0a836d222daafade39737e38f15
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-17 10:21:43 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,8 @@ set -x
 
 date
 
+git clone --depth 1 https://github.com/tshr20140816/heroku-mode-04.git self_repository &
+
 php -l www/redirect.php
 php -l loggly.php
 
@@ -14,6 +16,15 @@ mv phpPgAdmin-5.1 www/phppgadmin
 rm -f phpPgAdmin-5.1.tar.bz2
 
 cp config.inc.php www/phppgadmin/conf/config.inc.php
+
+wait
+
+pushd self_repository
+last_update=$(git log | grep Date)
+echo "<HTML><BODY>${last_update}</BODY></HTML>" > ../www/last_update.html
+popd
+
+rm -rf self_repository
 
 chmod 755 ./start_web.sh
 
