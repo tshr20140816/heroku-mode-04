ae7c7310c0087158c5dfacfc733398e74f35e22b
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-25 23:59:51 +0900

    Update fqdn.php

--- www/fqdn.php
+++ www/fqdn.php
@@ -5,6 +5,21 @@ $pdo = new PDO(
   "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
   $connection_info['user'],
   $connection_info['pass']);
-  
-  
+
+$sql = <<< __HEREDOC__
+SELECT M1.fqdn
+  FROM m_application M1
+ WHERE M1.select_type = 1
+   AND M1.dyno_quota <> -1
+ ORDER BY CAST(M1.dyno_used as numeric) / CAST(M1.dyno_quota as numeric)
+ LIMIT 1 OFFSET 0
+__HEREDOC__;
+foreach ($pdo->query($sql) as $row)
+{
+  header('Content-type: text/plain; charset=utf-8');
+  echo $row['fqdn'];
+  break;
+}
+$pdo = null;
+
 ?>
