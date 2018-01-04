47db36f124c93ffde42f5cda267d506fe264eb26
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-04 19:07:25 +0900

    Update redirect.php

--- www/redirect.php
+++ www/redirect.php
@@ -145,7 +145,7 @@ for ($i = 0; $i < count($servers); $i++)
   //error_log(var_export($data, true));
   foreach($data as $one_record)
   {
-    error_log(var_export($one_record['source_blob'], true));
+    error_log(var_export($one_record['updated_at'], true));
     break;
   }
   break;
