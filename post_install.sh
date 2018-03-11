dd3c0b235436a614d110bca076ef7be33f0cf9df
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-11 19:46:07 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,27 +4,29 @@ set -x
 
 date
 
-git clone --depth 1 https://github.com/tshr20140816/heroku-mode-04.git self_repository &
+export HOME2=${PWD}
+
+git clone --depth 1 https://github.com/tshr20140816/heroku-mode-04.git /tmp/self_repository &
 
 php -l www/redirect.php
 php -l loggly.php
 
-tar xf phpPgAdmin-5.1.tar.bz2
+# ***** phppgadmin *****
 
-mv phpPgAdmin-5.1 www/phppgadmin
-
-rm -f phpPgAdmin-5.1.tar.bz2
-
-cp config.inc.php www/phppgadmin/conf/config.inc.php
+pushd www
+git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
+cp ../config.inc.php phppgadmin/conf/
+cp ../Connection.php phppgadmin/classes/database/
+popd
 
 wait
 
-pushd self_repository
-last_update=$(git log | grep Date | grep -o "\w\{3\} .\+$")
-echo "${last_update}" > ../www/last_update.txt
-popd
+# ***** self_repository *****
 
-rm -rf self_repository
+pushd /tmp/self_repository
+last_update=$(git log | grep Date | grep -o "\w\{3\} .\+$")
+echo "${last_update}" > ${HOME2}/www/last_update.txt
+popd
 
 chmod 755 ./start_web.sh
 
