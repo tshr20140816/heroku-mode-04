5c73de0c84b07f58e178f8147a792c63b6b33a26
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-13 09:53:00 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -34,15 +34,17 @@ fi
 
 export HOME_IP_ADDRESS=$(nslookup ${HOME_FQDN} 8.8.8.8 | tail -n2 | grep -o '[0-9]\+.\+')
 
-echo "${HOME_FQDN} ${HOME_IP_ADDRESS}" > /app/HOME_IP_ADDRESS
-
-htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
-
 url="https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/START/"
 
 last_commit=$(curl -s https://github.com/tshr20140816/heroku-mode-03/commits/master.atom | grep Grit | grep -E -o Commit.+ | head -n 1)
 
 curl -i -H 'content-type:text/plain' -d "S ${last_commit:7:-5} heroku-mode-03" ${url}
+
+last_update=$(cat /app/www/last_update.txt)
+
+curl -i -H 'content-type:text/plain' -d "S ${HEROKU_APP_NAME} * ${HOME_FQDN} ${HOME_IP_ADDRESS} * ${last_update}"  ${url}
+
+htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
 # vendor/bin/heroku-php-apache2 -C apache.conf www
 rm apache.conf
