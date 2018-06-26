227158eaf7825e901a0285357f22d1e7b6f200a7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-26 09:53:46 +0900

    Update fqdn.php

--- www/fqdn.php
+++ www/fqdn.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!isset($_GET['n']) || $_GET['n'] === '' || is_array($_GET['n'])) {
+  $n = 0;
+} else {
+  $n = 1;
+}
+
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
   "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
@@ -14,6 +20,18 @@ SELECT M1.fqdn
  ORDER BY CAST(M1.dyno_used as numeric) / CAST(M1.dyno_quota as numeric)
  LIMIT 1 OFFSET 0
 __HEREDOC__;
+
+if ($n == 1) {
+  $sql = <<< __HEREDOC__
+SELECT M1.fqdn
+  FROM m_application M1
+ WHERE M1.select_type = 1
+   AND M1.dyno_quota <> -1
+ ORDER BY CAST(M1.dyno_used as numeric) / CAST(M1.dyno_quota as numeric)
+ LIMIT 1 OFFSET 1
+__HEREDOC__;
+}
+
 foreach ($pdo->query($sql) as $row)
 {
   header('Content-type: text/plain; charset=utf-8');
