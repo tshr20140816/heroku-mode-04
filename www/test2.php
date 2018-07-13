9e19b96688806170eec005e996c290b68f885fdd
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-13 10:31:50 +0900

    Update test2.php

--- www/test2.php
+++ www/test2.php
@@ -2,44 +2,12 @@
 
 $pid = getmypid();
 
-if (!isset($_GET['p']) || $_GET['p'] === '')
-{
-  error_log('IGNORE');
-  exit();
-}
-$path = $_GET['p'];
-
-if ($path !== 'ttrss' && $path !== 'ml')
-{
-  error_log('IGNORE');
-  exit();
-}
-
 $connection_info = parse_url(getenv('DATABASE_URL'));
 
 $pdo = new PDO(
   "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
   $connection_info['user'],
   $connection_info['pass']);
-
-// 未使用割合が最も多いサーバにリダイレクト
-
-$sql = <<< __HEREDOC__
-SELECT M1.fqdn
-  FROM m_application M1
- WHERE M1.select_type = 1
-   AND M1.dyno_quota <> -1
- ORDER BY CAST(M1.dyno_used as numeric) / CAST(M1.dyno_quota as numeric)
- LIMIT 1 OFFSET 0
-__HEREDOC__;
-
-foreach ($pdo->query($sql) as $row)
-{
-  $fqdn = $row['fqdn'];
-  break;
-}
-
-header("Location: https://${fqdn}/${path}/");
 
 // 使用量チェック & 更新
 
