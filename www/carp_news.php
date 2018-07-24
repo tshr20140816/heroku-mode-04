c31acace1046fa02deae053a4fd151905fb53d51
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-24 10:01:51 +0900

    Update carp_news.php

--- www/carp_news.php
+++ www/carp_news.php
@@ -2,6 +2,16 @@
 
 $content = file_get_contents('http://www.carp.co.jp/news18/index.html');
 
+$content = preg_replace('/<img.+?>/', '', $content);
+$content = preg_replace('/<link.+?>/', '', $content);
+$content = preg_replace('/<script.+?>/', '', $content);
+$content = str_replace('</script>', '', $content);
+$content = preg_replace('/<h1.+?<\/ul>/s', '', $content, 1);
+$content = preg_replace('/<!--.*?-->/s', '', $content);
+$content = preg_replace('/<body.+?>/', '<body>', $content, 1);
+$content = preg_replace('/^ +/m', '', $content);
+$content = preg_replace('/^ *\n/m', '', $content);
+
 echo $content;
 
 ?>
